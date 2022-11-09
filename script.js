d2e5a9bddb4f0252996559b6c8cf7450b210d528
Aline Mello <109696840+alinemello29@users.noreply.github.com>
2022-11-09 17:06:45 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const yourShip = document.querySelector('.player-shooter');
 const playArea = document.querySelector('#main-play-area');
-const aliensImg = ['monster-1.png', 'monster-2.png', 'monster-3.png'];
+const aliensImg = ['imagem/monster-1.png', 'imagem/monster-2.png', 'imagem/monster-3.png'];
 const instructionsText = document.querySelector('.game-instructions');
 const startButton = document.querySelector('.start-button');
 let alienInterval;
